c94a16ac79fa4e23bc4130011130bec6c747a136
Carlos Lopez <clopezo.externo@bcds.net>
2024-08-31 22:25:22 +0200

    base sveltekit app

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+	import './styles.css';
+</script>
+
+<div class="w-full h-full bg-gray-800">
+	<main>
+		<slot />
+	</main>
+
+	<footer class="flex text-white justify-center w-full">
+		<p class="text-lg">Desarrollado con SvelteKit por <a class="font-semibold" href="https://github.com/foxkdev">@Foxkdev</a> y <a href="https://github.com/jandriw">@Jandriw</a></p>
+	</footer>
+</div>
